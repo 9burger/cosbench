963988d81e07c4247ad74b0b7bde2465b230621a
maxnieberger <maxnieberger@ultrata.com>
2022-03-25 11:26:07 -0600

    Finish updating s3z to new aws sdk version

--- dev/cosbench-s3z/src/com/intel/cosbench/api/S3ZStor/S3ZStorage.java
+++ dev/cosbench-s3z/src/com/intel/cosbench/api/S3ZStor/S3ZStorage.java
@@ -3,6 +3,9 @@ package com.intel.cosbench.api.S3ZStor;
 import static com.intel.cosbench.client.S3ZStor.S3ZConstants.*;
 
 import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.Duration;
 
 import org.apache.http.HttpStatus;
 
@@ -14,13 +17,11 @@ import org.apache.http.HttpStatus;
 //import com.amazonaws.services.s3.model.*;
 //import com.amazonaws.services.s3.model.S3Object;
 
-// New AWS SDK
-import software.amazon.awssdk.*;
-import software.amazon.awssdk.auth.*;
-import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.http.apache.*;
 import software.amazon.awssdk.services.s3.*;
-import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.*;
 
 import com.intel.cosbench.api.storage.*;
@@ -30,6 +31,7 @@ import com.intel.cosbench.log.Logger;
 
 public class S3ZStorage extends NoneStorage {
 	private int timeout;
+	private Duration timeoutDuration;
 	
     private String accessKey;
     private String secretKey;
@@ -64,29 +66,73 @@ public class S3ZStorage extends NoneStorage {
 
         logger.debug("using storage config: {}", parms);
         
-//      Old method commented
-        ClientConfiguration clientConf = new ClientConfiguration();
-//        ClientOverrideConfiguration.Builder overrideConfig = ClientOverrideConfiguration.builder();
-//        ApacheHttpClient.Builder httpClientBuilder = ApacheHttpClient.builder();
-        clientConf.setConnectionTimeout(timeout);
-//        httpClientBuilder.connectionTimeout(timeout);
-        clientConf.setSocketTimeout(timeout);
-//        httpClientBuilder.socketTimeout(timeout);
-        clientConf.withUseExpectContinue(false);
-//        httpClientBuilder.expectContinueEnabled(false);
-        clientConf.withSignerOverride("S3SignerType");
-        clientConf.setProtocol(Protocol.HTTP);
-		if((!proxyHost.equals(""))&&(!proxyPort.equals(""))){
-			clientConf.setProxyHost(proxyHost);
-			clientConf.setProxyPort(Integer.parseInt(proxyPort));
+        
+        // Old configuration method
+//        ClientConfiguration clientConf = new ClientConfiguration();
+        
+//        clientConf.setConnectionTimeout(timeout);
+//        clientConf.setSocketTimeout(timeout);
+//        clientConf.withUseExpectContinue(false);
+        
+//        clientConf.withSignerOverride("S3SignerType");
+//        clientConf.setProtocol(Protocol.HTTP);
+        
+//		if((!proxyHost.equals(""))&&(!proxyPort.equals(""))){
+//			clientConf.setProxyHost(proxyHost);
+//			clientConf.setProxyPort(Integer.parseInt(proxyPort));
+//		}
+
+        // New Configuration
+        ApacheHttpClient.Builder httpClientBuilder = ApacheHttpClient.builder()
+        		.connectionTimeout(timeoutDuration)
+        		.socketTimeout(timeoutDuration)
+        		.expectContinueEnabled(false);
+        
+        // Unable to find v2 equivalent of S3SignerType, assume default is sufficient
+//        ClientOverrideConfiguration.Builder overrideConfig = ClientOverrideConfiguration.builder()
+//        		.advancedOption(SdkAdvancedClientOption.SIGNER);//"S3SignerType"
+        // protocol is set indirectly, by setting an http endpoint on the client builder
+        
+        if((!proxyHost.equals(""))&&(!proxyPort.equals(""))){
+        	
+            ProxyConfiguration.Builder proxyConfig = ProxyConfiguration.builder();
+        	int portNum = Integer.parseInt(proxyPort);
+        	
+        	try {
+        		URI proxyEndpointURI = new URI(null,null,proxyHost,portNum,null,null,null);
+        		
+        		proxyConfig.endpoint(proxyEndpointURI);
+        		httpClientBuilder.proxyConfiguration(proxyConfig.build());
+        		
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
         
-        AWSCredentials myCredentials = new BasicAWSCredentials(accessKey, secretKey);
-        client = new AmazonS3Client(myCredentials, clientConf);
-        client.setEndpoint(endpoint);
-        client.setS3ClientOptions(new S3ClientOptions().withPathStyleAccess(pathStyleAccess));
         
-        logger.debug("S3 client has been initialized");
+		// Old client creation
+//        AWSCredentials myCredentials = new BasicAWSCredentials(accessKey, secretKey);
+//        client = new AmazonS3Client(myCredentials, clientConf);
+//        client.setEndpoint(endpoint);
+//        client.setS3ClientOptions(new S3ClientOptions().withPathStyleAccess(pathStyleAccess));
+
+        AwsBasicCredentials myCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+        S3ClientBuilder clientBuilder = S3Client.builder()
+        		.credentialsProvider(StaticCredentialsProvider.create(myCredentials))
+        		.httpClientBuilder(httpClientBuilder)
+        		.serviceConfiguration(S3Configuration.builder().pathStyleAccessEnabled(pathStyleAccess).build());
+		try {
+			URI endpointURI = new URI(endpoint);
+			clientBuilder.endpointOverride(endpointURI);
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+        client = clientBuilder.build();
+		
+        logger.debug("S3 client V2 has been initialized");
     }
     
     @Override
@@ -98,6 +144,22 @@ public class S3ZStorage extends NoneStorage {
 //        } catch (Exception e) {
 //            throw new StorageException(e);
 //        }
+    }
+    
+    private boolean bucketExists(String container) {
+    	try {
+    		// doesBucketExist replaced by headBucket exception
+        	HeadBucketRequest bucketExistsCheck = HeadBucketRequest.builder()
+        			.bucket(container)
+        			.build();
+        	
+        	client.headBucket(bucketExistsCheck);
+        	return true;
+        	
+        } catch (NoSuchBucketException e) {
+        	// Bucket DNE
+        	return false;
+        }
     }
 
     @Override
@@ -111,9 +173,11 @@ public class S3ZStorage extends NoneStorage {
         super.getObject(container, object, config);
         InputStream stream;
         try {
+        	// Old method
+//            S3Object s3Obj = client.getObject(container, object);
+//            stream = s3Obj.getObjectContent();
         	
-            S3Object s3Obj = client.getObject(container, object);
-            stream = s3Obj.getObjectContent();
+        	stream = client.getObject(GetObjectRequest.builder().bucket(container).key(object).build());
             
         } catch (Exception e) {
             throw new StorageException(e);
@@ -124,14 +188,28 @@ public class S3ZStorage extends NoneStorage {
     @Override
     public void createContainer(String container, Config config) {
         super.createContainer(container, config);
+        // Old Method
+//        try {
+//        	if(!client.doesBucketExist(container)) {
+//	        	
+//	            client.createBucket(container);
+//        	}
+//        } catch (Exception e) {
+//            throw new StorageException(e);
+//        }
+        
         try {
-        	if(!client.doesBucketExist(container)) {
+        	if(!bucketExists(container)) {
+	        	CreateBucketRequest bucketRequest = CreateBucketRequest.builder()
+	                    .bucket(container)
+	                    .build();
 	        	
-	            client.createBucket(container);
+	        	client.createBucket(bucketRequest);
         	}
         } catch (Exception e) {
-            throw new StorageException(e);
+        	throw new StorageException(e);
         }
+
     }
 
 	@Override
@@ -139,11 +217,19 @@ public class S3ZStorage extends NoneStorage {
             long length, Config config) {
         super.createObject(container, object, data, length, config);
         try {
-    		ObjectMetadata metadata = new ObjectMetadata();
-    		metadata.setContentLength(length);
-    		metadata.setContentType("application/octet-stream");
-    		
-        	client.putObject(container, object, data, metadata);
+        	// Old method
+//    		ObjectMetadata metadata = new ObjectMetadata();
+//    		metadata.setContentLength(length);
+//    		metadata.setContentType("application/octet-stream");
+//    		
+//        	client.putObject(container, object, data, metadata);
+        	
+        	client.putObject(PutObjectRequest.builder()
+                    .bucket(container)
+                    .key(object)
+                    .build(),
+                    RequestBody.fromInputStream(data, length));
+        	
         } catch (Exception e) {
             throw new StorageException(e);
         }
@@ -152,24 +238,47 @@ public class S3ZStorage extends NoneStorage {
     @Override
     public void deleteContainer(String container, Config config) {
         super.deleteContainer(container, config);
+        // Old method
+//        try {
+//        	if(client.doesBucketExist(container)) {
+//        		client.deleteBucket(container);
+//        	}
+//        } catch(S3Exception awse) {
+//        	if(awse.statusCode() != HttpStatus.SC_NOT_FOUND) {
+//        		throw new StorageException(awse);
+//        	}
+//        } catch (Exception e) {
+//            throw new StorageException(e);
+//        }
+        
         try {
-        	if(client.doesBucketExist(container)) {
-        		client.deleteBucket(container);
-        	}
-        } catch(S3Exception awse) {
-        	if(awse.statusCode() != HttpStatus.SC_NOT_FOUND) {
-        		throw new StorageException(awse);
-        	}
-        } catch (Exception e) {
-            throw new StorageException(e);
-        }
+	    	if(bucketExists(container)) {
+	    		DeleteBucketRequest bucketRequest = DeleteBucketRequest.builder()
+                    .bucket(container)
+                    .build();
+	    		
+	    		client.deleteBucket(bucketRequest);
+	    	}
+	    } catch(S3Exception awse) {
+	    	if(awse.statusCode() != HttpStatus.SC_NOT_FOUND) {
+	    		throw new StorageException(awse);
+	    	}
+	    } catch (Exception e) {
+	        throw new StorageException(e);
+	    }
     }
 
     @Override
     public void deleteObject(String container, String object, Config config) {
         super.deleteObject(container, object, config);
         try {
-            client.deleteObject(container, object);
+        	DeleteObjectRequest objectRequest = DeleteObjectRequest.builder()
+        			.bucket(container)
+        			.key(object)
+        			.build();
+        	
+            client.deleteObject(objectRequest);
+            
         } catch(S3Exception awse) {
         	if(awse.statusCode() != HttpStatus.SC_NOT_FOUND) {
         		throw new StorageException(awse);
